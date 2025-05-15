7a4aeb9aff12bbfbe2ef5c2f1a7fe1019149e93b
Deepak1999 <73517127+Deepak1999@users.noreply.github.com>
2025-05-15 10:46:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import MainLayout from './Components/MainLayout/MainLayout';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import Dashboard from './Components/Dashboard/Dashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={<MainLayout><Dashboard /></MainLayout>} />}
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
